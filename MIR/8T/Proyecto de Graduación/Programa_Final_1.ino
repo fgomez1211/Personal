302b9b9de37ecf9a063db6df413df4d4223dfbd5
Fernando Gómez <98927828+fgomez1211@users.noreply.github.com>
2023-11-13 20:10:47 -0600

    Update Programa_Final_1.ino
    Código realizado con Zea, 13 de Noviembre 2023.
    Arduino MEGA. Movimiento e Interrupts, 1 Canal del Encoder.

--- MIR/8T/Proyecto de Graduación/Programa_Final_1.ino
+++ MIR/8T/Proyecto de Graduación/Programa_Final_1.ino
@@ -1,43 +1,90 @@
 
 
-const byte ledPin = 13;
-const byte buttonPin =2;
-int motor = 7;
-int counter=0;
-int relacion_encoder=512;
+byte signalPin =18;
+//byte signalPin_2 =3;
 
 
+volatile long int contador_A=0;
+volatile long int contador_B=0;
+
+//PMW Motores y Enables
+
+int pwm1 = 4;
+int pwm2 = 5;
+int enable1 = 2;
+int enable2 = 3;
 
 
-volatile bool toogleState =false;
+//Pasos del encoder y relaciones entre el encoder y los engranajes.
+const int pasos_encoder=512;
+double relacion_eje_1=214.13;
+
+
+//Pasos para una rotación completa de cada articulacion
+double relacion_encoder_1=pasos_encoder*relacion_eje_1;
+
+//Coordenadas de prueba en grados
+double  grados_eslabon_1 =30;
+
+//Coordenadas en pasos
+double pasos_1 = (grados_eslabon_1/360) * relacion_encoder_1;
 
 
 void setup(){
-  pinMode(ledPin, OUTPUT);
-  pinMode(motor,OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), checkSwitch, CHANGE);
+
+  //Señal del Encoder
+  pinMode(signalPin, INPUT_PULLUP);
+
+
+  //Declaración de las variables del encoder
+  pinMode(enable1, OUTPUT);
+  pinMode(enable2, OUTPUT);
+  pinMode(pwm1,OUTPUT);
+  pinMode(pwm2,OUTPUT);
+
+
+
+  attachInterrupt(digitalPinToInterrupt(signalPin), enc_A, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(buttonPin), enc_index, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(buttonPin_2), enc_home, CHANGE); 
+
+  //Activa los Enable del driver, dejando los PWM  para enceder/apagar el motor.
+  digitalWrite(enable1, HIGH); 
+  digitalWrite(enable2, HIGH);
+  digitalWrite(pwm2,LOW);e
+
 }
 
 
 void loop(){
-  if (counter <5){
-    digitalWrite(7,HIGH);
-    Serial.println(counter);
+
+
+  if (contador_A < pasos_1){
+    digitalWrite(pwm2,HIGH);
+    Serial.println(contador_A);
+    Serial.println(pasos_1);
   }
     else{
-      digitalWrite(7,LOW);
+      digitalWrite(pwm2,LOW);
+      Serial.println("Motor Apagado");
     }
 }
 
 
-
-void checkSwitch(){
-  if (digitalRead(buttonPin) == LOW){
-    counter++;
-  }
+void enc_A(){
+    contador_A++;
 }
 
+
+
+//void enc_index(){
+  //if (digitalRead(signalPin) == LOW){
+    //contador_A++;
+  //}
+//}
+
+
+// */
   
     
