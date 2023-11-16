de9af62f5a768612b33eba8930a2a447d0299300
Fernando Gómez <98927828+fgomez1211@users.noreply.github.com>
2023-11-15 19:28:48 -0600

    Actualización 15112023
    Actualización 15112023 - Movimiento posicionado y reseteado.

--- MIR/8T/Proyecto de Graduación/Programa_Test.ino
+++ MIR/8T/Proyecto de Graduación/Programa_Test.ino
@@ -1,76 +1,66 @@
 
+//Señales del Encoder
+byte signalPin =18;
+byte signalPin_2 =20;
+byte posicion_actual=0;
 
-const byte signalPin =2;
-const byte signalPin_2 =3;
 
-int contador_A=0;
-int contador_B=0;
+volatile long int contador_A=0;
+volatile long int contador_B=0;
 
 //PMW Motores y Enables
-
-int pwm1 = 8;
-int pwm2 = 9;
-int enable1 = 10;
-int enable2 = 11;
+int pwm1 = 4;
+int pwm2 = 5;
+int enable1 = 2;
+int enable2 = 3;
 
 
 //Pasos del encoder y relaciones entre el encoder y los engranajes.
-const int pasos_encoder=30;
-const int relacion_eje_1=214;
-const int relacion_eje_2=244;
-const int relacion_eje_3=213;
-const int relacion_eje_4=180;
-const int relacion_eje_5=180;
+const int pasos_encoder=512;
+double relacion_eje_1=214.13;
 
 
 //Pasos para una rotación completa de cada articulacion
-float relacion_encoder_1=pasos_encoder*relacion_eje_1;
-float relacion_encoder_2=pasos_encoder*relacion_eje_2;
-float relacion_encoder_3=pasos_encoder*relacion_eje_3;
-float relacion_encoder_4=pasos_encoder*relacion_eje_4;
-float relacion_encoder_5=pasos_encoder*relacion_eje_5;
-
+double relacion_encoder_1=pasos_encoder*relacion_eje_1;
 
 //Coordenadas de prueba en grados
-const int  grados_eslabon_1 =3;
-const int  grados_eslabon_2 =30;
-const int  grados_eslabon_3 =30;
-const int  grados_eslabon_4 =30;
-const int  grados_eslabon_5 =30;
-
-
+double  grados_eslabon_1 =30;
 
 //Coordenadas en pasos
-float pasos_1 = grados_eslabon_1 * relacion_encoder_1;
-float pasos_2 = grados_eslabon_2 * relacion_encoder_2;
-float pasos_3 = grados_eslabon_3 * relacion_encoder_3;
-float pasos_4 = grados_eslabon_4 * relacion_encoder_4;
-float pasos_5 = grados_eslabon_5 * relacion_encoder_5;
-
-
-
+double pasos_1 = (grados_eslabon_1/360) * relacion_encoder_1;
 
 
 void setup(){
+  Serial.begin(9600);
+
+  //Señal del Encoder
   pinMode(signalPin, INPUT_PULLUP);
+
+
+  //Declaración de las variables del encoder
   pinMode(enable1, OUTPUT);
   pinMode(enable2, OUTPUT);
   pinMode(pwm1,OUTPUT);
   pinMode(pwm2,OUTPUT);
+
+
+
+  attachInterrupt(digitalPinToInterrupt(signalPin), enc_A, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(buttonPin), enc_index, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(signalPin_2), enc_index, FALLING); 
+
+  //Activa los Enable del driver, dejando los PWM  para enceder/apagar el motor.
   digitalWrite(enable1, HIGH); 
   digitalWrite(enable2, HIGH);
-  digitalWrite(pwm1, LOW);
   digitalWrite(pwm2,LOW);
-
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(signalPin), enc_A, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(buttonPin), enc_index, FALLING);
-  //attachInterrupt(digitalPinToInterrupt(buttonPin_2), enc_home, CHANGE);  
+  digitalWrite(pwm1,LOW);
 
 }
 
 
 void loop(){
+
+
   if (contador_A < pasos_1){
     digitalWrite(pwm1,HIGH);
     Serial.println(contador_A);
@@ -78,23 +68,18 @@ void loop(){
   }
     else{
       digitalWrite(pwm1,LOW);
+      double Posicion_actual = (contador_A/relacion_encoder_1) * 360;
+      Serial.print("Posicion_actual: " );
+      Serial.println(Posicion_actual);
+      delay(5000);
+      contador_A=0;
+
     }
 }
 
 
 void enc_A(){
-  if (digitalRead(signalPin) == LOW){
     contador_A++;
-  }
 }
 
-void enc_index(){
-  if (digitalRead(signalPin) == LOW){
-    contador_A++;
-  }
-}
-
-
-// */
-  
     
